9b20c3eebe065ba94485ddfd477172ec018d775f
clarane <claraputrias@gmail.com>
2018-12-07 20:13:32 +0700

    reverting

--- sido/resources/views/layouts/navbar.blade.php
+++ sido/resources/views/layouts/navbar.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of c3c9b9b... FE auth
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -43,6 +46,7 @@
   <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
+<<<<<<< HEAD
 
 <body class="hold-transition skin-blue sidebar-mini">
   <div class="wrapper">
@@ -141,11 +145,94 @@
   <script src="{{ url ('asset/dist/js/pages/dashboard.js') }}"></script>
   <!-- AdminLTE for demo purposes -->
   <script src="{{ url ('asset/dist/js/demo.js" ') }}"></script>
+=======
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+  <header class="main-header">
+    <!-- Logo -->
+    <a href="index2.html" class="logo greener">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>K</b></span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><b>KOMSI</b></span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top greener">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <!-- Account -->
+          <li class="dropdown user user-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <img src="{{ url ('asset/dist/img/user2-160x160.jpg') }}" class="user-image" alt="User Image">
+              <span class="hidden-xs">Alexander Pierce</span>
+            </a>
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <!-- Left side column. contains the logo and sidebar -->
+  @yield('sidebar')
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    @yield('content')
+  </div>
+  <!-- /.content-wrapper -->
+    @yield('footer')
+
+  <!-- Control Sidebar -->
+
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 3 -->
+<script src="{{ url ('asset/bower_components/jquery/dist/jquery.min.js') }}"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="{{ url ('asset/bower_components/jquery-ui/jquery-ui.min.js') }}"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button);
+</script>
+<!-- Bootstrap 3.3.7 -->
+<script src="{{ url ('asset/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<!-- Morris.js charts -->
+<script src="{{ url ('asset/bower_components/raphael/raphael.min.js') }}"></script>
+<script src="{{ url ('asset/bower_components/morris.js/morris.min.js') }}"></script>
+<!-- Sparkline -->
+<script src="{{ url ('asset/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js') }}"></script>
+<!-- jvectormap -->
+<script src="{{ url ('asset/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
+<script src="{{ url ('asset/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+<!-- jQuery Knob Chart -->
+<script src="{{ url ('asset/bower_components/jquery-knob/dist/jquery.knob.min.js') }}"></script>
+<!-- daterangepicker -->
+<script src="{{ url ('asset/bower_components/moment/min/moment.min.js') }}"></script>
+<script src="{{ url ('asset/bower_components/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
+<!-- datepicker -->
+<script src="{{ url ('asset/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') }}"></script>
+<!-- Bootstrap WYSIHTML5 -->
+<script src="{{ url ('asset/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}"></script>
+<!-- Slimscroll -->
+<script src="{{ url ('asset/bower_components/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>
+<!-- FastClick -->
+<script src="{{ url ('asset/bower_components/fastclick/lib/fastclick.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ url ('asset/dist/js/adminlte.min.js') }}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{ url ('asset/dist/js/pages/dashboard.js') }}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{ url ('asset/dist/js/demo.js"') }}"></script>
+>>>>>>> parent of c3c9b9b... FE auth
 <!-- DataTables -->
 <script src="asset/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
 <script src="asset/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
 <script>
     $(function () {
+<<<<<<< HEAD
       $('#example1 ').DataTable()
       $('#example2 ').DataTable({
         'paging '      : true,
@@ -154,8 +241,22 @@
         'ordering '    : true,
         'info '        : true,
         'autoWidth '   : false
+=======
+      $('#example1').DataTable()
+      $('#example2').DataTable({
+        'paging'      : true,
+        'lengthChange': false,
+        'searching'   : false,
+        'ordering'    : true,
+        'info'        : true,
+        'autoWidth'   : false
+>>>>>>> parent of c3c9b9b... FE auth
       })
     })
   </script>
 </body>
+<<<<<<< HEAD
 </html>
+=======
+</html>
+>>>>>>> parent of c3c9b9b... FE auth
